237645d1dd18a3916b2b25dad38109e37aa4c53b
oanhptl <oanhpham99x@gmail.com>
2020-01-02 07:41:22 +0700

    update

---
+++ loop/app.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el:'#vue-app',
+    data: {
+        name:['oanh','trang','ly','kien','an','thuy'],
+        student: [{
+            id: 101,
+            name : "kien"
+        }, {
+            id: 102,
+            name : "oanh"
+        }
+    ]
+    }
+})
